9ab2d680d3b0b6c219e1cd4fabbbcd6987e8c3aa
AtulLohani <atull.evision@gmail.com>
2024-05-29 16:45:40 +0530

    inital commit

---
+++ javascript_programs/Practice.js
@@ -0,0 +1,583 @@
+// // // var a = 10
+// // // var a = 20
+// // // // var a = 5
+// // // console.log(a+a+a)
+
+// // // let b = 20
+// // // let b = 30
+// // // console.log(b+c)
+
+// // // const a = 500
+// // // const b = 500
+// // // console.log(a+b)
+// // // for (let i=0;i<10;i++){
+// // //     console.log("or kya bade")
+// // // }
+
+// // for (let i=5;i<51;i=i+5){
+// //     console.log(i)
+// // }
+
+// // // let age = prompt('Enter Age')
+// // // if (age >= 18){
+// // //     console.log('You can drive')
+// // // }else console.log('you cant drive')
+// // // let age = prompt('enter age')
+// // //  age >= 18 ? console.log('You can drive') : console.log('You cant drive')
+
+// // let num = 20
+// // let result = num % 2 ? 'odd' : 'even'
+// // console.log(result)
+
+// // const ColdStorage = {
+// //     potato: 500,
+// //     coriander: 20,
+// //     onion: "nahi hai",
+// //     corrot: false
+// //     }
+
+// //     ColdStorage ["potato"] = ColdStorage ["potato"] +3000
+// //     console.log(ColdStorage["potato"])
+
+// // // create a constant object called "prduct" to store information shown in the picture you picked from anywhere
+
+// // const Product = {
+// //     name : "parker jotter",
+// //     price : 270,
+// // }
+// // console.log(Product.name)
+
+// // // arithmetice opertors in js
+// // let c = 20
+// // let d = 30
+// // console.log(c+d)
+// // console.log(c*d)
+// // console.log(c/d)
+// // console.log(c-d)
+// // // modulus
+// // console.log(c%d)
+// // // expontentiation
+// // console.log(c**d)
+
+// // // unary oprators
+// // let x = 20
+// // let y  = 30
+// // x++
+// // y--
+// // console.log(x)
+// // console.log(y)
+
+// // // /assignment opreoters
+// // let z = 20
+// // let v  = 30
+// // z += 10
+// // // v -= 10
+// // v *= 2
+// // console.log(z,v)
+
+// // // comparison operators
+// // let num1 = 5
+// // let num2 = 6
+// // console.log(num1 == num2)    //  equal to false
+// // console.log(num1 !== num2)   // not equal to true
+
+// // // logical operators
+// // let cond1 = num1 < num2;
+// // let cond2 = 5 === 6;
+// // console.log(cond1 && cond2)  // logical AND false (when all value are true it gives true else false)
+// // console.log(cond1 || cond2)  // logical OR  true (when atleast one condition true this gives true)
+// // console.log(! (5 === 6))  // logical NOT  true  (always gives opposite)
+
+// // // conditional statement
+// // let age = 5
+// // if (age < 18){
+// //     console.log("you are not eligible to vote")
+// // }else console.log("you are eligible to vote")
+
+// // let mode =  "dark";
+// // let color;
+// // if(mode == "dark"){
+// //     color="white"
+// // }
+// // if(mode == 'white'){
+// //     color="black"
+// // }
+// // if(mode == 'black'){
+// //   color="blue"
+// // }
+// // else console.log("no color")
+// // console.log(color)
+
+// // let nummm = 1
+// // if (nummm % 2 == 0) {
+// //     console.log("even")
+// // }else console.log("odd")
+
+// // let year = 2025
+// // if (year % 4 == 0){
+// //     console.log("leap year")
+// // }else console.log("normal year")
+
+// // // get  user to input a number . check if the number is a multiple of 5 or not
+// // let numbrr = 50
+// // if (numbrr % 5 == 0){
+// //     console.log("multiple of 5")
+// // }else console.log("no multiply of 5")
+
+// // // write a code which can gives grades to students according to their scores
+// // let score = 39
+// // if (score > 90 && score < 100){
+// //     console.log("A")
+// // }
+// // else if (score > 80 && score < 89){
+// //     console.log("B")
+// // }
+// // else if (score > 70 && score < 79){
+// //     console.log("C")
+// // }
+// // else if (score > 60 && score < 69){
+// //     console.log("D")
+// // }
+// // else if (score < 60 && score > 40){
+// //     console.log("E")
+// // }
+// // else console.log("fail ho gaya bhai")
+
+// //  loops
+// // for (i = 1; i <= 101; i++) {
+// //   console.log("jai shree raam");
+// // }
+
+// // let sum = 0;
+// // for (let i = 1; i <= 50; i++) {
+// //   sum = sum + i;
+// // }
+// // console.log(sum);
+
+// // for of loop example
+// // let str = "atul lohani";
+// // for (let i of str) {
+// //   console.log(i);
+// // }
+
+// //  for in loop example
+// // let studentsss = {
+// //   name: "atul",
+// //   age: 20,
+// //   cgpa: 102,
+// //   address: "indore",
+// // };
+
+// // for (let key in studentsss) {
+// //   console.log(key, studentsss[key]);
+// // }
+
+// // print all even numbers from 0 to 100 ?
+// // for (j = 1; j <= 100; j++)
+// //   if (j % 2 == 0) {
+// //     console.log(j);
+// //   }
+
+// // create a game where you start with any random game number ask the user to keep guessing the game number until the user enters correct value?
+// // let gameNum = 100
+// // let userNum = prompt ("guess the number");
+// // while (userNum != gameNum) {
+// //     userNum = prompt ("guess the correct number");
+// // }
+// // console.log("congrates right number");
+
+// // string methods
+
+// // let strinnnnc = "atul lohani";
+// // let newStrin = strinnnnc.toUpperCase();
+// // console.log(strinnnnc)
+// // console.log(newStrin)
+
+// // let strinnnnct = "ATUL LOHANI";
+// // let newStrinn = strinnnnct.toLowerCase();
+// // console.log(strinnnnct)
+// // console.log(newStrinn)
+
+// // arrays
+
+// // let marks =  [20,30,40,50,60,70,80]
+// // console.log(marks)
+
+// // looping over an array
+// // let friends = ['pratik','pranay','aman','shubham','dipesh']
+// // for (let i=0; i < friends.length; i++){
+// //   console.log(friends[i])
+// // }
+
+// //  using for of loop
+// // let Cities = ['indore','dewas','ujjain','bhopal']
+// // for (let ele of Cities){
+// //   console.log(ele.toUpperCase())}
+
+// // for a given array with marks of student -> [95,97,44,37,76,60] find the average marks
+// // let mar = [20,40,50,60,40,50,80,90,78,79,62,45]
+// // summm = 0
+// // for (let numsss of mar)
+// //   summm = summm + numsss
+// // console.log(summm/mar.length)
+
+// // given array [250,645,300,900,50]
+// // item have 10% discount. change the array to store a final  price after applying offer.
+// // using for of
+// // let price = [1000,2000,3000,4000,5000];
+// // let k = 0;
+// // for (let val of price){
+// //   let offer  = val/10 ;
+// //   price[k] = price[k] - offer;
+// //   console.log(price[k]);
+// //   k++;
+// // }
+
+// // using for
+// // let digits = [200,300,400,500,600,700,800,900,1000]
+// // for (let b = 0; b < digits.length; b++){
+// //   let offer = digits[b]/10;
+// //   digits[b] -= offer
+// // }
+// // console.log(digits)
+
+// // array methods
+// // let djs = ['speakar','console','eps','monitor']
+// // djs.push('headphones')
+// // djs.pop('eps')
+// // console.log(djs.toString())
+// // console.log(djs.slice(1,4s))
+// // console.log(djs.splice(2))
+
+// // let company = ['thoughtwin','evision','bestpeers','cashify','amazon']
+// // // company.shift();
+// // company.splice(3,1,"bridgefixx")
+// // company.push('netflix')
+// // console.log(company)
+
+// // let int = 40
+// // for (let int = 0; int <= 40; int++){
+// // console.log(int)
+// // }
+
+// // let intnum = prompt("enter a number")
+// // if (intnum % 2 == 0){
+// //   console.log('even number')
+// // }else {console.log('odd number')}
+
+// // functions and methods
+
+// // prompt('hello bade bhai')
+
+// function myFunc() {
+//   console.log("hello world");
+//   console.log("learn something");
+// }
+// myFunc();
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// // let myVal = sum (500,1500)
+// // console.log(myVal)
+
+// const arrowSum = (a, b) => {
+//   console.log(a + b);
+// };
+
+// // create a function using the "function" keyword that takes a string as an argument & returns the number of vowels in the string ?
+
+// function countVowel(str) {
+//   let count = 0;
+//   for (let char of str) {
+//     if (
+//       char == "a" ||
+//       char == "e" ||
+//       char == "i" ||
+//       char == "o" ||
+//       char == "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// // const alphabets = prompt("Enter a word:");
+// // const alphacount = countVowel(word);
+// // console.log(`The word "${alphabets}" contains ${alphacount} vowels.`)
+
+// // crate a arrow function performing same task
+// const countVow = (str) => {
+//   let count = 0;
+//   for (let char of str) {
+//     if (
+//       char == "a" ||
+//       char == "e" ||
+//       char == "i" ||
+//       char == "o" ||
+//       char == "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+// // const word = prompt("Enter a word:");
+// // const vowelCount = countVow(word);
+// // console.log(`The word "${word}" contains ${vowelCount} vowels.`)
+// let random = "asdfghjkasdfghjk".toUpperCase()
+// console.log(random)
+
+// // for each  in array
+// const country = ['india','thailand','nepal','bangladesh']
+// country.forEach((value,index)=>{
+//   console.log(value.toUpperCase(),index)
+// })
+
+// // for a given array of number, print the square of each value using the for each loop.
+// const numbe = [2,4,6,8,10];
+// numbe.map((char,index)=>{
+//   console.log(char*char,index)
+// })
+
+// // filter method
+// let digitss = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+// let even = digitss.filter((valuee)=>{
+//   return valuee < 3
+// });
+// console.log(even);
+
+// // reduce method
+// let sankhya = [20,400,500,600,4584,45,1,2,3]
+// let countttt = sankhya.reduce((bada,chota)=>{
+//   return chota < bada ?  chota : bada
+// });
+// console.log(countttt)
+
+// // we are given array of marks of students. filter our of the marks of student that scored 90+.
+// let nu = [90,80,70,60,50,40,30,20,10,100,500,300];
+// let filterMarks = nu.filter((valui)=>{
+//   return valui < 90
+// });
+// console.log(filterMarks)
+
+// // let n = prompt ('enter a number');
+// // let aarr = [];
+// // for (let l = 1; l <= n; l++){
+// //   aarr[l - 1] = l;
+// // }
+// // console.log(aarr)
+
+// // let sem = aarr.reduce((res,curr)=> {
+// //   return res + curr;
+// // }) ;
+// // console.log("sum",sem)
+
+// // let factorial = aarr.reduce((res,curr)=>{
+// //   return res * curr;
+// // })
+// // console.log("factorial",factorial)
+
+// // let para = document.querySelector('p');
+// // console.log(para.setAttribute('class','new'))
+
+// // let p = document.querySelector('p');
+// // p.append("button");
+
+// let nayiHead = document.createElement("h1");
+// nayiHead.innerHTML = "<em>hello world i am learning javascript ! </em>";
+// document.querySelector("body").prepend(nayiHead)
+
+// // create a button element. give it a text "click me",backgrund color of red and text color of white.
+// // insert the button as a first element inside a body tag.
+
+// let khatka = document.createElement('button');
+// khatka.innerText = "click me";
+// khatka.style.backgroundColor = "red";
+// khatka.style.color = "white";
+// document.querySelector("body").prepend(khatka)
+
+// // create a "p" tag in html, give it a class & some styling.
+// // now create  a new class in css and try to append this class to the <p> element.
+// // did you notice, how you overwrite the class name when you add a new  one ?
+// // solve this problem using class list.
+
+// // let para = document.querySelector("p");
+// // para.classList.add("newClass")
+
+// let btn1 = document.querySelector("#btn1");
+// btn1.addEventListener("click", (e) => {
+//   console.log("Cancel button was clicked");
+//   console.log(e.type)
+// });
+// btn1.addEventListener("click", (e) => {
+//   console.log("Cancel button was clicked-handler 2");
+//   console.log(e.type)
+// });
+// // btn1.addEventListener("click", (e) => {
+// //   console.log("Cancel button was clicked-handler 2");
+// //   console.log(e.type)
+// // });
+
+// const handler2 = () => {
+//   console.log("Cancel button was clicked-handler 2");
+// }
+
+// btn1.removeEventListener("click",handler2)
+
+// let box = document.querySelector(".box");
+// box.onmouseover = () => {
+//   console.log("you are inside")
+// }
+
+// // create a toggle button that changes the screen to dark mode when clicked & light-mode when clicked again.
+
+// let clrchnge = document.querySelector("#clrchnge");
+// let currMode = "orange";
+
+// clrchnge.addEventListener("click",() => {
+//   if(currMode === "orange") {
+//     currMode = "green"
+//     document.querySelector("body").style.backgroundColor = "orange"
+//   } else {
+//     currMode = "orange";
+//     document.querySelector("body").style.backgroundColor = "green"
+//   }
+//   console.log(currMode)
+// });
+
+// what is prototype and object ?
+
+// const student = {
+//   fullName: "atul lohani",
+//   marks: 50,
+//   printMarks: function () {
+//     console.log(this.marks);
+//   },
+// };
+
+// const employee = {
+//   calcTax() {
+//     console.log("10 per.tax");
+//   },
+// };
+// const atulSalary = {
+//   salary: 10000,
+// };
+// const harshilSalary = {
+//   salary: 20000,
+// };
+
+// const ankitSalary = {
+//   salary: 30000,
+// };
+
+// const pavanSalary = {
+//   salary: 40000,
+// };
+
+// atulSalary.__proto__ = employee;
+
+
+// class HyundaiCar  {
+//   constructor (brand,mileage) {
+//     console.log("creating new object")
+//     this.brand = brand
+//     this.mileage = mileage
+//   }
+//   start () {
+//     console.log('start');
+//   }
+
+//   stop () {
+//     console.log('stop');
+//   }
+
+//   setBrand(brand){
+//     this.brand = brand;
+//   }
+// }
+
+// let Eon = new HyundaiCar('Eon',20);
+// let i20 = new HyundaiCar();
+// // Eon.setBrand(Eon)
+
+
+// class person {
+//   constructor(name){
+//     this.species = "homo's"
+//     this.name = name
+
+//   }
+//   eat () {
+//     console.log("eat")
+//   }
+//   sleep () {
+//     console.log("sleep")
+//   }
+// }
+
+// class Developer extends person {
+//    constructor(name){
+//     super(name);
+//   }
+//   work(){
+//     super.eat();
+//     console.log("do some coding !")
+//   }
+// }
+// let honeyObj = new Developer("honey");
+
+// Developer.extends(person);
+
+// error handling in js
+// function asyncfunc () {
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('data Encrypted 975958471...., Pratik Kotiya... more data fetching.....');
+//       resolve("waiting for more data....")
+//     },2000);
+//   });
+// }
+// console.log("Fetching Pratik's Instagram..")
+// let p1 = asyncfunc();
+// p1.then((res) => {
+//   console.log(res);
+// }) 
+
+// // async and await 
+// async function getData(id) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log(`data ${id} received`);
+//       resolve();
+//     }, 1000);
+//   });
+// }
+
+
+// async function getAllData (){
+//   console.log("getting data 1")
+//   await getData(1);
+//   console.log("getting data 2")
+//   await getData(2);
+//   console.log("getting data 3")
+//   await getData(3);
+//   console.log("getting data 4")
+//   await getData(4);
+// }
+
+const url = "https://jsonplaceholder.typicode.com/users";
+const factPara = document.querySelector("#fact");
+
+let promise = fetch(url);
+console.log(promise)
+
+const facts = async() => {
+  console.log('getting data...');
+  let response = await fetch (url);
+  console.log(response.status);
+  let data = await response.json();
+  factPara.innerHTML = data[0].text;
+};
